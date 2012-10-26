d1247a9e01848c01aed1107462c9fe109c7a167b
R. Tyler Croy <tyler@monkeypox.org>
2012-10-26 15:03:39 -0700

    Clone tasseo onto disk, still failing my cukes

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -4,4 +4,21 @@ class tasseo::debian {
       alias  => 'git',
       ensure => present;
   }
+
+  file {
+    '/srv' :
+      ensure => directory;
+  }
+
+  exec {
+    'clone tasseo' :
+      command => 'git clone git://github.com/obfuscurity/tasseo.git',
+      unless  => 'test -f /srv/tasseo/.git/config',
+      cwd     => '/srv',
+      path    => ['/bin', '/usr/bin'],
+      require => [
+        Package[git],
+        File['/srv']
+      ];
+  }
 }
--- spec/classes/debian_spec.rb
+++ spec/classes/debian_spec.rb
@@ -4,5 +4,13 @@ describe 'tasseo::debian' do
   include_context 'debian system'
 
   it { should contain_package('git-core') }
+  it { should contain_file('/srv') }
+
+  it 'should clone tasseo onto disk' do
+    should contain_exec('clone tasseo').with({
+      'cwd'     => '/srv',
+      'require' => ['Package[git]', 'File[/srv]']
+    })
+  end
 
 end
